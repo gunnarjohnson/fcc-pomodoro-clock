9091c749e4a1a4b24a6e3ae9dc7701fb6ebc63b6
Gunnar Johnson <gunnarjohnson.github@gmail.com>
2018-07-20 18:02:15 -0500

    Add react-boilerplate

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactComponent from './components/ReactComponent';
+import './styles/styles.css';
+
+ReactDOM.render(<ReactComponent />, document.getElementById('app'));
---
+++ webpack.common.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './src/index.html'})  
+  ]
+};

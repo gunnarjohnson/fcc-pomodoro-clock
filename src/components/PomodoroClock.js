620d9f71e6ad30c2657f769bf8ae153b07435dfe
Gunnar Johnson <gunnarjohnson.github@gmail.com>
2018-07-24 15:45:28 -0500

    Add Babel plugin for ES6 class support and modify React components

--- src/components/PomodoroClock.js
+++ src/components/PomodoroClock.js
@@ -5,100 +5,92 @@ import Session from './Session';
 import Timer from './Timer';
 
 class PomodoroClock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.startTimer = this.startTimer.bind(this);
-    this.resetTimer = this.resetTimer.bind(this);
-    this.breakDecrement = this.breakDecrement.bind(this);
-    this.breakIncrement = this.breakIncrement.bind(this);
-    this.sessionDecrement = this.sessionDecrement.bind(this);
-    this.sessionIncrement = this.sessionIncrement.bind(this);
-		this.state = {
-      minutes: 25,
-      seconds: undefined,
-      breakTime: 5,
-      sessionTime: 25,
-      timerInactive: true
-		};
-	}
-
-  resetSeconds() {
-    if (this.state.seconds !== undefined) {
-      this.setState({
-        seconds: undefined
-      });
-    }
-  }
-
-  startTimer() {
-    if (this.state.timerInactive) {
-      this.setState({ timerInactive: false });
-
-      let timeLeft = setInterval(() => {
-        if (this.state.seconds == undefined) {
-          this.setState({ 
-            minutes: this.state.minutes - 1,
-            seconds: 59 
-          });
-        }
-        else if (this.state.seconds == 1) {
-          this.setState({ 
-            seconds: undefined
-          });
-        }
-        else {
-          this.setState({ 
-            seconds: this.state.seconds - 1
-          });
-        }
-      }, 1000);
-    } else {
-      this.setState({
-        timerInactive: true
-      });
-      // stopTimer();
-    }
-    console.log('Start/Stop Timer');
+	state = {
+    minutes: 25,
+    seconds: undefined,
+    breakInactive: true,
+    breakTime: 5,
+    sessionTime: 25,
+    timerInactive: true
   };
 
-  resetTimer() {
+  startTimer = () => {
+    if (this.state.timerInactive) {
+      this.setState({ timerInactive: false });
+      this.interval = setInterval(() => {
+
+        if (this.state.seconds == undefined) {
+          if (this.state.minutes == 0  && this.state.breakInactive) {
+            this.setState({ 
+              minutes: this.state.breakTime,
+              breakInactive: false
+            });
+          } else if (this.state.minutes == 0) {
+            this.setState({
+              minutes: this.state.sessionTime,
+              breakInactive: true
+            });
+          } else {
+            this.setState({ 
+              minutes: this.state.minutes - 1,
+              seconds: 59,
+            });
+          }
+
+        } else if (this.state.seconds == 1) {
+          this.setState({ seconds: undefined });
+
+        } else {
+          this.setState({ seconds: this.state.seconds - 1 });
+        }
+
+      }, 1000);
+    } else {
+      this.setState({ timerInactive: true });
+      clearInterval(this.interval);
+    }
+  };
+
+  resetTimer = () => {
     this.setState({
       minutes: 25,
       seconds: undefined,
+      breakInactive: true,
       breakTime: 5,
-      sessionTime: 25
+      sessionTime: 25,
+      timerInactive: true
     });
-    console.log('Reset Timer');
   };
-  breakDecrement() {
+
+  breakDecrement = () => {
     if (this.state.breakTime > 1) {
       this.setState({ breakTime: this.state.breakTime - 1 });
-    } else {
-      console.log('Limit reached');
     }
   };
-  breakIncrement() {
+
+  breakIncrement = () => {
     if (this.state.breakTime < 60) {
       this.setState({ breakTime: this.state.breakTime + 1 });
-    } else {
-      console.log('Limit reached');
     }
   };
-  sessionDecrement() {
+
+  sessionDecrement = () => {
     if (this.state.sessionTime > 1) {
       this.setState({ 
         minutes: this.state.sessionTime - 1,
+        seconds: undefined,
         sessionTime: this.state.sessionTime - 1 
       });
-    } else {
-      console.log('Limit reached');
     }
   };
-  sessionIncrement() {
+
+  sessionIncrement = () => {
     if (this.state.sessionTime < 60) {
-      this.setState({ sessionTime: this.state.sessionTime + 1 });
-    } else {
-      console.log('Limit reached');
+      this.setState({ 
+        minutes: this.state.sessionTime + 1,
+        seconds: undefined,
+        sessionTime: this.state.sessionTime + 1 
+      });
     }
   };
   
@@ -107,6 +99,7 @@ class PomodoroClock extends React.Component {
       <div>
         <Header />
         <Timer
+          breakInactive={this.state.breakInactive}
           minutes={this.state.minutes}
           seconds={this.state.seconds}
           startTimer={this.startTimer}
